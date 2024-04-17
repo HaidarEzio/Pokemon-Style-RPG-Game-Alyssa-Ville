1a03bf3c70977c5297dceafa05b7a54aa8fb03be
Alyssa Favorito <alyssafavorito2000@gmail.com>
2024-04-17 18:00:30 -0400

    Alyssa-Ville set-up

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("This is my game");
+// resize canvas to be larger
+const canvas = document.getElementById("game-container");
+canvas.width = 1024;
+canvas.height = 576;
+console.log(canvas);

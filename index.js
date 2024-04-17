dd3ec3ce7dd4319033f2181a9a5db34dc7e1400c
Alyssa Favorito <alyssafavorito2000@gmail.com>
2024-04-17 19:19:39 -0400

    create sprite class

--- index.js
+++ index.js
@@ -19,11 +19,34 @@ image.src = '/RPGGameMapAlyssaVille_noSprites.png'
 const playerImage = new Image();
 playerImage.src = "/playerDown.png"
 
-// need to wait to load image before trying to draw image
-// load map before player so that player is on top of map
-image.onload = () => {
-  c.drawImage(image, -690, -380);
-  c.drawImage(
+class Sprite {
+    constructor({position, image, velocity}) {
+        this.position = position;
+        this.image = image;
+    }
+
+    draw() {
+        c.drawImage(image, -690, -380)
+    }
+}
+
+const background = new Sprite({
+    // set position to an object with x and y axis
+    position: {
+    x: -690,
+    y: -380
+    },
+    image: image
+})
+
+// animation infinite loop
+function animate() {
+
+    window.requestAnimationFrame(animate)
+    
+    // load map before player so that player is on top of map
+    background.draw(image)
+    c.drawImage(
     playerImage, 
     // cropping arguments (x, y, crop width, crop height)
     0, 
@@ -37,14 +60,9 @@ image.onload = () => {
     // last two arguments are width and height that image should be rendered out as
     playerImage.width/4,
     playerImage.height
-    );  
+    )  
 }
-
-// animation loop
-function animate() {
-    window.requestAnimationFrame(animate)
-    console.log("animate")
-}
+window.requestAnimationFrame(animate);
 // listen for when player presses key and execute function
 window.addEventListener('keydown', (e) => {
     // console.log("keydown works", e.key)
